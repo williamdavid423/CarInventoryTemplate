0e7690a3ab7bfed9d438b189b36c5c3c4c135cab
billy david <williamdavid423@hotmail.com>
2022-03-01 10:00:13 -0500

    inventory guy

---
+++ CarInventory/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInventory
+{
+    internal class Car
+    {
+       public int year;
+       public string make;
+       public string colour;
+        public double mileage;
+
+            public Car (int _year, string _make, string _colour, double _mileage)
+        {
+            year = _year;
+            make = _make;
+            colour = _colour;
+            mileage = _mileage;
+        }
+
+    }
+}
--- CarInventory/Form1.cs
+++ CarInventory/Form1.cs
@@ -11,6 +11,7 @@ namespace CarInventory
 {
     public partial class Form1 : Form
     {
+        List<Car> cars = new List<Car>();
 
         public Form1()
         {
@@ -19,6 +20,15 @@ namespace CarInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            
+            int year = Convert.ToInt32(yearInput.Text);
+            string make = makeInput.Text;
+            string colour = colourInput.Text;
+            double mileage = Convert.ToDouble(mileageInput.Text);
+
+               Car c = new Car(year, make, colour, mileage);
+            cars.Add(c);
+
 
         }
     }
